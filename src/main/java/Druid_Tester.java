f2a26a2ba4cf5df582ce80489a1f244477b502ce
codermomo <79038671+codermomo@users.noreply.github.com>
2022-01-23 19:59:12 +0800

    Update Druid_Tester.java

--- src/main/java/Druid_Tester.java
+++ src/main/java/Druid_Tester.java
@@ -16,11 +16,11 @@ public class Druid_Tester {
         adapter.initConnect(ip, port, user, password);
 
         // header in output file
-        DruidAdapter.appendToFile("Druid DB Query.csv", "\"Trial\",\"Q1\",\"Q2(Q3)\",\"Q3(Q4)\"\n");
+        DruidAdapter.appendToFile("Druid DB Query.csv", "\"Trial\",\"Q1a\",\"Q1b\",\"Q1c\",\"Q2(Q3)\",\"Q3(Q4)\"\n");
 
         System.out.println("Run " + numLoops + " loops (Q1 -> Q2 -> Q3 -> Q1 -> ...):");
 
-        Double avgQ1 = 0.0, avgQ2 = 0.0, avgQ3 = 0.0;
+        Double avgQ1a = 0.0, avgQ1b = 0.0, avgQ1c = 0.0, avgQ2 = 0.0, avgQ3 = 0.0;
 
         for (int i = 0; i < numLoops; i++) {
             System.out.println("~trial " + i + "~");
@@ -28,9 +28,15 @@ public class Druid_Tester {
             DruidAdapter.appendToFile("Druid DB Query.csv", "\"" + i + "\",");
             // print Q1
             // Double query1result = query1(adapter);
-            Double query1result = 0.0;
-            avgQ1 += query1result;
-            DruidAdapter.appendToFile("Druid DB Query.csv", "\"" + query1result + "\",");
+            Double query1aresult = query1a(adapter);
+            avgQ1a += query1aresult;
+            DruidAdapter.appendToFile("Druid DB Query.csv", "\"" + query1aresult + "\",");
+            Double query1bresult = query1b(adapter);
+            avgQ1b += query1bresult;
+            DruidAdapter.appendToFile("Druid DB Query.csv", "\"" + query1bresult + "\",");
+            Double query1cresult = 0.0;
+            avgQ1c += query1cresult;
+            DruidAdapter.appendToFile("Druid DB Query.csv", "\"" + query1cresult + "\",");
             // print Q2
             Double query2result = query2(adapter);
             avgQ2 += query2result;
@@ -41,10 +47,11 @@ public class Druid_Tester {
             DruidAdapter.appendToFile("Druid DB Query.csv", "\"" + query3result + "\"\n");
         }
 
-        avgQ1 /= numLoops;
+        avgQ1a /= numLoops;
         avgQ2 /= numLoops;
         avgQ3 /= numLoops;
-        DruidAdapter.appendToFile("Druid DB Query.csv", "\"avg\",\"" + avgQ1 + "\",\"" + avgQ2 + "\",\"" + avgQ3 + "\"\n");
+        DruidAdapter.appendToFile("Druid DB Query.csv", "\"avg\",\"" + avgQ1a +
+                "\",\"" + avgQ1b + "\",\"" + avgQ1c + "\",\"" + avgQ2 + "\",\"" + avgQ3 + "\"\n");
 
         System.out.println("Finished all trials");
 
@@ -56,9 +63,35 @@ public class Druid_Tester {
         return "Time spent for receiving respond (not ingestion) is " + time;
     }
 
-    private static Double query1(BaseAdapter adapter) {
+    private static Double query1a(BaseAdapter adapter) {
         try {
-            return adapter.query1();
+            return adapter.query1a();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+        return 0.0;
+    }
+
+    private static Double query1b(BaseAdapter adapter) {
+        try {
+            return adapter.query1b();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+        return 0.0;
+    }
+
+    private static Double query1c(BaseAdapter adapter) {
+        try {
+            return adapter.query1c();
         } catch (SQLException e) {
             e.printStackTrace();
             System.exit(-1);
